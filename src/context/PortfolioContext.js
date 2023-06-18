84c20f9f8348c299f008b9012cca6850223564c2
Stefan Pusica <pusicastefan1@gmail.com>
2023-06-18 21:10:28 +0200

    Update links and hero text

--- src/context/PortfolioContext.js
+++ src/context/PortfolioContext.js
@@ -1,139 +1,144 @@
-import { createContext, useEffect, useState } from 'react'
-import Cookies from 'js-cookie'
-import images from '../constants/images'
-import { fab as brandIcons } from '@fortawesome/free-brands-svg-icons';
+import { createContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import images from "../constants/images";
+import { fab as brandIcons } from "@fortawesome/free-brands-svg-icons";
 import { faPalette, faDatabase } from "@fortawesome/free-solid-svg-icons";
 
-const PortfolioContext = createContext()
+const PortfolioContext = createContext();
 
-export const PortfolioProvider = ({children}) => {
-    const [portfolioItems, setPortfolioItems] = useState([
-        {
-            id: 1,
-            logo: `${images.brakumLogo}`,
-            subject: 'Brakum',
-            aboutEng: 'One page presentational web site. The main purpose was to bring activity and services of car-fleeting and operating leases to users. Design of the project was done in Figma. The site was made in React. Sending and receiving Email is done using the EmailJS plugin.',
-            about: 'Jednostranični prezentacioni web sajt čija je svrha približiti korisnicima delatnost i usluge car-fleeta i operativnog lizinga. Dizajn projekta rađen je u Figmi. Sajt je izrađen pomoću React-a. Slanje i primanje Emaila realizovano je pomoću dodatka EmailJS.',
-            technologies: 'React',
-            mockup: `${images.BrakumMockup}`,
-            siteUrl: 'http://brakum.rs/',
-            showCardDetalis: false
-        },
-        {
-            id: 2,
-            logo: `${images.cottoLogo}`,
-            subject: 'Cotto',
-            aboutEng: 'One page presentational web site for restaurant in Belgrade. Design was implemented on Figma. Interaction with Email is realised by extension EmailJS, interaction with WhatsApp is realised with ReactWhatsApp.  The site was made in React. ',
-            about: 'Jednostranični prezentacioni web sajt jednog beogradskog restorana. Rađen po ugledu na figma dizajn. Zbog mnoštva detalja, interakcija sa Email-om realizovana je pomoću doadtka EmailJS, dok je interakciju sa WhatsApp-om zadužen ReactWhatsApp dodatak. Sajt je urađen pomoću React-a.',
-            technologies: 'React',
-            mockup: `${images.CottoMockup}`,
-            siteUrl: 'https://cotto-update.netlify.app/',
-            showCardDetalis: false
-        },
-        {
-            id: 3,
-            logo: `${images.marasCoastLogo}`,
-            subject: 'Maras Coast',
-            aboutEng: 'Multipage web site with the purpose of promoting a tourist accommodation. The site was made in React.',
-            about: 'Višestranični prezentacioni sajt čija svrha promocija turističkog smeštaja. Sajt je urađen pomoću React-a.',
-            technologies: 'React',
-            mockup: `${images.MarasMockup}`,
-            siteUrl: 'https://marascoast.com/',
-            showCardDetalis: false
-        },
-        {
-            id: 4,
-            logo: `${images.tbeautyLogo}`,
-            subject: 'TBeauty',
-            aboutEng: 'Multipage web site for a beauty salon which is not fully completed. Technologies used HTML, CSS, JavaScript.',
-            about: 'Višestranični prezentacioni sajt salona lepote koji nije u potpunosti završen. Rađen je po ugledu lično kreirani dizajn u tehnologijama HTML, CSS i JavaScript.',
-            technologies: 'HTML, CSS, JavaScript',
-            mockup: `${images.TbeautyMockup}`,
-            siteUrl: 'https://tamara-beauty-update.netlify.app/index.html',
-            showCardDetalis: false
-        }
-    ])
+export const PortfolioProvider = ({ children }) => {
+  const [portfolioItems, setPortfolioItems] = useState([
+    {
+      id: 1,
+      logo: `${images.brakumLogo}`,
+      subject: "Brakum",
+      aboutEng:
+        "One page presentational web site. The main purpose was to bring activity and services of car-fleeting and operating leases to users. Design of the project was done in Figma. The site was made in React. Sending and receiving Email is done using the EmailJS plugin.",
+      about:
+        "Jednostranični prezentacioni web sajt čija je svrha približiti korisnicima delatnost i usluge car-fleeta i operativnog lizinga. Dizajn projekta rađen je u Figmi. Sajt je izrađen pomoću React-a. Slanje i primanje Emaila realizovano je pomoću dodatka EmailJS.",
+      technologies: "React",
+      mockup: `${images.BrakumMockup}`,
+      siteUrl: "http://brakum.rs/",
+      showCardDetalis: false,
+    },
+    {
+      id: 2,
+      logo: `${images.cottoLogo}`,
+      subject: "Cotto",
+      aboutEng:
+        "One page presentational web site for restaurant in Belgrade. Design was implemented on Figma. Interaction with Email is realised by extension EmailJS, interaction with WhatsApp is realised with ReactWhatsApp.  The site was made in React. ",
+      about:
+        "Jednostranični prezentacioni web sajt jednog beogradskog restorana. Rađen po ugledu na figma dizajn. Zbog mnoštva detalja, interakcija sa Email-om realizovana je pomoću doadtka EmailJS, dok je interakciju sa WhatsApp-om zadužen ReactWhatsApp dodatak. Sajt je urađen pomoću React-a.",
+      technologies: "React",
+      mockup: `${images.CottoMockup}`,
+      siteUrl: "https://gotgotit.netlify.app/",
+      showCardDetalis: false,
+    },
+    {
+      id: 3,
+      logo: `${images.marasCoastLogo}`,
+      subject: "Maras Coast",
+      aboutEng:
+        "Multipage web site with the purpose of promoting a tourist accommodation. The site was made in React.",
+      about:
+        "Višestranični prezentacioni sajt čija svrha promocija turističkog smeštaja. Sajt je urađen pomoću React-a.",
+      technologies: "React",
+      mockup: `${images.MarasMockup}`,
+      siteUrl: "https://marascoast.com/",
+      showCardDetalis: false,
+    },
+    {
+      id: 4,
+      logo: `${images.tbeautyLogo}`,
+      subject: "TBeauty",
+      aboutEng:
+        "Multipage web site for a beauty salon which is not fully completed. Technologies used HTML, CSS, JavaScript.",
+      about:
+        "Višestranični prezentacioni sajt salona lepote koji nije u potpunosti završen. Rađen je po ugledu lično kreirani dizajn u tehnologijama HTML, CSS i JavaScript.",
+      technologies: "HTML, CSS, JavaScript",
+      mockup: `${images.TbeautyMockup}`,
+      siteUrl: "https://tbeauty.netlify.app/",
+      showCardDetalis: false,
+    },
+  ]);
 
-    const [skills, setSkills] = useState([
-        {
-            techIcon: brandIcons.faReact,
-            techSubject: 'Frontend',
-            techSubjectSR: 'Frontend',
-            techSkills: [
-                'HTML',
-                'CSS',
-                'Sass',
-                'Bootstrap',
-                'Tailwind',
-                'JavaScript',
-                'React',
-                'Wordpress',
-                'Woocommerce',
-                'Elementor'
-            ]  
-        },
-        {
-            techIcon: faDatabase,
-            techSubject: 'Database',
-            techSubjectSR: 'Baza Podataka',
-            techSkills: [
-                'Firebase'
-            ]  
-        },
-        {
-            techIcon: faPalette,
-            techSubject: 'Design',
-            techSubjectSR: 'Dizajn',
-            techSkills: [
-                'Photoshop',
-                'Figma',
-            ]  
-        }
-    ])
+  const [skills, setSkills] = useState([
+    {
+      techIcon: brandIcons.faReact,
+      techSubject: "Frontend",
+      techSubjectSR: "Frontend",
+      techSkills: [
+        "HTML",
+        "CSS",
+        "Sass",
+        "Bootstrap",
+        "Tailwind",
+        "JavaScript",
+        "React",
+        "Wordpress",
+        "Woocommerce",
+        "Elementor",
+      ],
+    },
+    {
+      techIcon: faDatabase,
+      techSubject: "Database",
+      techSubjectSR: "Baza Podataka",
+      techSkills: ["Firebase"],
+    },
+    {
+      techIcon: faPalette,
+      techSubject: "Design",
+      techSubjectSR: "Dizajn",
+      techSkills: ["Photoshop", "Figma"],
+    },
+  ]);
 
-    const [portfolioFlagItem, setPortfolioFlagItem] = useState({
-        itemReady: {},
-        edit: false
-    })
+  const [portfolioFlagItem, setPortfolioFlagItem] = useState({
+    itemReady: {},
+    edit: false,
+  });
 
-    // Set item to be updated
-    const showDetalis = (itemReady) => {
-        setPortfolioFlagItem({
-            itemReady,
-            edit: true
-        })
-    }
+  // Set item to be updated
+  const showDetalis = (itemReady) => {
+    setPortfolioFlagItem({
+      itemReady,
+      edit: true,
+    });
+  };
 
-    // Close Detalis
-    const closeDetalis = () => {
-        setPortfolioFlagItem({
-            itemReady: {},
-            edit: false
-        })
-    }
+  // Close Detalis
+  const closeDetalis = () => {
+    setPortfolioFlagItem({
+      itemReady: {},
+      edit: false,
+    });
+  };
 
-    // //////////////
-    // Translate
+  // //////////////
+  // Translate
 
-    const [english, setEnglish] = useState(true)
+  const [english, setEnglish] = useState(true);
 
-    const setLanguange = () => {
-        setEnglish(!english)
-    }
+  const setLanguange = () => {
+    setEnglish(!english);
+  };
 
-
-    return <PortfolioContext.Provider value={{
+  return (
+    <PortfolioContext.Provider
+      value={{
         portfolioItems,
         portfolioFlagItem,
         skills,
         english,
         showDetalis,
         closeDetalis,
-        setLanguange
-        
-    }}>
-        {children}
+        setLanguange,
+      }}
+    >
+      {children}
     </PortfolioContext.Provider>
-}
+  );
+};
 
 export default PortfolioContext;
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,32 +1,40 @@
-import React, { useContext } from 'react';
-import './Hero.css'
+import React, { useContext } from "react";
+import "./Hero.css";
 // import Cookies from 'js-cookie'
-import PortfolioContext from '../../context/PortfolioContext'
-import SectionTitle from '../../components/SectionTitle/SectionTitle'
-import SectionText from '../../components/SectionText/SectionText'
-import ButtonMultyColorHref from '../../components/ButtonMultiplyColorHref/ButtonMultiplyColorHref'
-import HeroAnimation from '../../components/HeroAnimation/HeroAnimation'
+import PortfolioContext from "../../context/PortfolioContext";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import SectionText from "../../components/SectionText/SectionText";
+import ButtonMultyColorHref from "../../components/ButtonMultiplyColorHref/ButtonMultiplyColorHref";
+import HeroAnimation from "../../components/HeroAnimation/HeroAnimation";
 
 function Hero() {
-
-  const {english} = useContext(PortfolioContext)
+  const { english } = useContext(PortfolioContext);
 
   // let cookieValue = Cookies.get('switch') // => 'value'
 
   return (
-    <div className="hero" id='Hero'>
-        <div className="hero-holder">
-            <div className="hero-left-side">
-                <SectionTitle text={english  ? 'Welcome!' : 'Dobro došli!'} />
-                <SectionText text={english ? `In addition to the acquired faculty education I was improving my knowledge and skills  with books and professional internet content. I am motivated to learn and improve my skills` : `Pored stečenog fakultetskog obrazovanja usavršavao sam svoja znanja i veštine uz pomoć knjiga i stručnih internet sadržaja. Motivisan sam da učim i unapređujem svoje veštine`  } />
-                <ButtonMultyColorHref buttonHrefTo={'#Skills'} buttonText={english ? 'Detalis' : 'Detaljnije'} />
-            </div>
-            <div className="hero-right-side">
-              <HeroAnimation className="heroAnimation" />
-            </div>
+    <div className="hero" id="Hero">
+      <div className="hero-holder">
+        <div className="hero-left-side">
+          <SectionTitle text={english ? "Welcome!" : "Dobro došli!"} />
+          <SectionText
+            text={
+              english
+                ? `I am a seasoned web application developer with over a year of industry experience. Currently, I am employed at MTS sistemi i integracije, where I am involved in the development of one of the most popular OTT(Over-the-top media service) applications in the Balkans.`
+                : `Iskusan sam programer web aplikacija s više od godinu dana radnog iskustva. Trenutno sam zaposlen u MTS sistemi i integracije, gde radim na razvoju jedne od najpopularnijih OTT(Over-the-top media service) aplikacija na Balkanu.`
+            }
+          />
+          <ButtonMultyColorHref
+            buttonHrefTo={"#Skills"}
+            buttonText={english ? "Detalis" : "Detaljnije"}
+          />
         </div>
+        <div className="hero-right-side">
+          <HeroAnimation className="heroAnimation" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
